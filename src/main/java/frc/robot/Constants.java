c9048463e445a3921210722c1c131aba106f2c5f
TomasC <tomassjchoi@gmail.com>
2021-11-13 14:48:40 -0800

    DriveTrain and TankDrive Revised.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,12 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+
+    public static final int LeftDriveTalonPort = 2;
+    public static final int RightDriveTalonPort =3;
+    public final class White_DriveTrainPorts{
+        
+    };
+}
+    
